eccfcd440e2a83ff128ee342c78bd0371d904a88
athh <athziri.rdgz@gmail.com>
2021-12-01 21:10:35 -0600

    Create amazon_main.js

---
+++ pageObjects/amazon_main.js
@@ -0,0 +1,11 @@
+module.exports = {
+    url: 'https://www.amazon.com/',
+    elements: {
+        menu_right:{
+            selector: '.nav-right'
+        },
+        start_here_link:{
+            selector: '#nav-flyout-ya-newCust'
+        }
+    }
+}
